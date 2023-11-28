0af78adab54b6194675b355ed8ff437e31a3f62b
Petr Saparov <saparov.petr@yandex.ru>
2023-11-28 20:13:45 +0300

    28-11-23

--- js/script.js
+++ js/script.js
@@ -12,12 +12,6 @@
     let randomElement = getRandomElement(phrases); //в переменную randomElement записывается результат выполения функции getRandomElement  (другими словами: рандомный возвращенный элемент массива записывается в переменную RandomElement)
     smoothly(phrase, 'textContent', randomElement.text);//плавно проявляется на экране текст из переменной randomElement (точнее производится замена на экране захваченного элемента phrase - его свойство textContent меняется на текст из переменной randomElement)
     smoothly(image, 'src', randomElement.image); //плавно проявляется на экране картинка из переменной randomElement (указазывается подлежащий изменению элемент страницы, свойство элемента, новое значение свойства) 
-  
-    if (randomElement.text.length > 40) {
-      advice.style.fontSize = '33px';
-    } else {
-      advice.style.fontSize = '42px';
-    }
   });
   
   for (let i = 0; i <= 2; i = i + 1) { 
